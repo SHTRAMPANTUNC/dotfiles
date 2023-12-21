48da83b1415854a97adeaf16a120e094963a4d98
SHTRAMPANTUNC <youngtuctim@gmail.com>
2023-12-21 01:30:10 +0000

    two commit

---
+++ fish/config.fish
@@ -0,0 +1,12 @@
+alias l="ls -all"
+alias cls="clear"
+alias vim="nvim"
+alias f="feh -g 1920x1080 -d -S filename"
+alias w-on="wg-quick up $HOME/.kupr_desktop.conf"
+alias w-off="wg-quick down $HOME/.kupr_desktop.conf"
+
+set fish_greeting
+
+if status is-interactive
+    # neofetch
+end
