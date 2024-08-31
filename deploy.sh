e4432ca279bc05bc5501348bd0588f07d7c2d01f
Timur <74539640+SHTRAMPANTUNC@users.noreply.github.com>
2024-08-31 15:48:01 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,6 @@ NC='\033[0m'
 GITHUB_REPO="https://github.com/SHTRAMPANTUNC/dotfiles"
 CONFIG_DIR="$HOME/.config"
 
-set -e
-
 if git clone ${GITHUB_REPO} temp_repo; then
   echo -e "${GREEN}[ INFO ]:$1${NC} Repository successfully cloned"
 else
@@ -16,13 +14,13 @@ else
   exit 1
 fi
 
-if cp -r temp_repo/* $CONFIG_DIR/; then
-  echo -e "${GREEN}[ INFO ]:$1${NC} Config files successfully moving"
+if cp -r temp_repo/.config $CONFIG_DIR/ && cp .* $HOME/; then
+  echo -e "${GREEN}[ INFO ]:$1${NC} Dotfiles successfully moving"
 else
-  echo -e "${GREEN}[ ERROR ]:$1${NC} Error during move config files"
+  echo -e "${GREEN}[ ERROR ]:$1${NC} Error during move dotfiles"
   exit 1
 fi
 
 rm -rf temp_repo
 
-echo -e "${GREEN}[ FINISH ]:$1${NC} Deploy has been complited :D"
+echo -e "${GREEN}[ FINISH ]:$1${NC} Deploy has been completed :D"
